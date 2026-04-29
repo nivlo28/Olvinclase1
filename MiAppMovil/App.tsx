247ad9c91469dc98b1cb379d7043a5193da9aacd
nivlo28 <efrenhilton1980@gmail.com>
2026-04-29 16:58:55 -0600

    App-CustomButton

--- MiAppMovil/App.tsx
+++ MiAppMovil/App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <CustomButton 
+      title={"App"}
+       onPress={()=>{
+        console.log('Press desde boton App');
+      }}
+      /> 
+      <CustomButton 
+      title={"Secondary Button"}
+       onPress={()=>{
+        console.log('Press desde boton Secundario');
+      }}/>
     </View>
   );
 }
@@ -13,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e30505',
     alignItems: 'center',
     justifyContent: 'center',
   },
