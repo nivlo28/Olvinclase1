704a21e3be9e6718156b9c015ca0991eb2cc855a
nivlo28 <efrenhilton1980@gmail.com>
2026-05-13 17:30:27 -0600

    input
    implementacion de boton de esconder y mostrar 'eye'
    tambien useState

--- MiAppMovil/src/components/CustomInput.tsx
+++ MiAppMovil/src/components/CustomInput.tsx
@@ -1,5 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
-import { TextInput, TouchableOpacity, View,Text } from "react-native";
+import { useState } from "react";
+import { TextInput, TouchableOpacity, View,Text,StyleSheet } from "react-native";
 
 type Props = {
     type: "text" | "email" | "password" | "number";
@@ -9,20 +10,65 @@ type Props = {
 }
 
 export default function CustomInput({type,placeholder,value,onChange}: Props){
-    return(
-        <View>
+    const [isSecureText, setIsSecureText]=useState(true);
+    const icon : typeof Ionicons["name"] | undefined=
+        type === "email" ? 'at' : 
+          type === "password" ? 'lock-closed' : undefined
+
+              return(
+        //wrapped
+        <View style={styles.wrapper}>
+        <View style={styles.inputContainer}>
+            <Ionicons name= {icon as any}  size={20} color="#00000"/>
             <TextInput
                placeholder={placeholder}
                value={value}
                onChangeText={onChange}
+               style={styles.input}
+               secureTextEntry={true}
             />
-
-
-
-            <TouchableOpacity>
-               <Text>Icon Button </Text>
-               <Ionicons name={"eye"} size={15}/>
+            <TouchableOpacity
+            onPress={
+                ()=>{
+                   setIsSecureText(!isSecureText)
+                } 
+            }
+            >
+               <Ionicons name={isSecureText ? "eye" : "eye-off"} size={22}/>
             </TouchableOpacity>
+        </View>
+        <Text>
+          Probando Wrapper
+        </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+     wrapper: {
+     marginBottom: 10,
+ },
+    
+   inputContainer: {
+    //distribucion de componentes
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+
+
+    //estilizacion de input
+     borderColor: 'gray',
+     borderWidth: 1,
+     borderRadius: 9,
+     backgroundColor: 'white',
+     paddingHorizontal: 15,
+     paddingLeft: 20,
+
+   },
+   input:{
+    width: '70%',
+    paddingVertical: 10,
+    paddingHorizontal:10,
+   }
+
+})
